f13c97961826dcda5759a3ff05594a9914299099
rogalew <rogalew@fit.vutbr.cz>
2023-02-08 16:17:24 +0100

    vata2mata

--- vata2mata/vata2mata.sh
+++ vata2mata/vata2mata.sh
@@ -12,3 +12,30 @@ if [ ! -f "$ALPHA_MAP" ]; then
 	exit
 else true; fi
 
+if `grep "^Automaton .*" $1> /dev/null`; then true;
+else
+	echo "Broken Timbuk format in $1"
+	exit
+fi
+
+grep "^Automaton .*" $1 | sed "s/^Automaton *\(.*\)$/%Section t\1 NFA Bits/"
+
+init=`grep "^x *->" $1 | sed "s/.* ->\(.*\)$/\1/"`
+echo "%InitialFormula $init"
+
+final=`grep "^Final State" $1 | sed "s/^Final States \(.*\)$/\1/"`
+final=`echo $final | sed "s/\([^ ]\)  *\([^ ]\)/\1 | \2/g"`
+echo "%FinalFormula $final"
+
+substitute_line () {
+	while read a; do
+		symbol=`echo $a | sed "s/^ *\([^ (][^ (]*\).*/\1/"`
+		state2=`echo $a | sed "s/^.*-> *\([^ ][^ ]*\) */\1/"`
+		state1=`echo $a | sed "s/^.*( *\([^ )][^ )]\) *).*/\1/"`
+		bit_symbol=`grep "^$symbol:" "$ALPHA_MAP" | sed "s/^.*://"`
+		echo $state1 $bit_symbol $state2
+
+	done
+}
+
+grep "(..*) *->" $1 | substitute_line
